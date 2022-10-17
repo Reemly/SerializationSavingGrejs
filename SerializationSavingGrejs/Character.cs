86eef3c96bc17ba2d90e0ee0487e9443e8a004fc
Olle Järvheden <jarvheden.olle@elev.ga.ntig.se>
2022-10-17 13:38:23 +0200

    rgfg

---
+++ SerializationSavingGrejs/Character.cs
@@ -0,0 +1,8 @@
+using System;
+
+public class Character {
+    public int Health { get; set; }
+    public string EyeColor { get; set; }
+    public float JumpHeight { get; set; }
+
+}
---
+++ SerializationSavingGrejs/Terraria.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+using System.Text.Json;
+
+Character Terrarian = new Character();
+
+Terrarian.Health = 100;
+Terrarian.EyeColor = "yellow";
+Terrarian.JumpHeight = 2;
+
+
+
