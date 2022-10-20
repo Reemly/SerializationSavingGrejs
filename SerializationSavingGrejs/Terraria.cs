671c2099e9abea937304e74b83a06bb330df35a7
Olle Järvheden <jarvheden.olle@elev.ga.ntig.se>
2022-10-20 11:06:22 +0200

    giuyerg

--- SerializationSavingGrejs/Terraria.cs
+++ SerializationSavingGrejs/Terraria.cs
@@ -4,8 +4,18 @@ using System.Text.Json;
 Character Terrarian = new Character();
 
 Terrarian.Health = 100;
-Terrarian.EyeColor = "yellow";
+Terrarian.EyeColor = "";
 Terrarian.JumpHeight = 2;
 
+Console.WriteLine("");
 
+//DU MÅSTE SKRIVA ELLER GÖRA SÅ ATT ANVÄNDAREN KAN SPARA DERAS EGNA VÄRDEN!!
+
+string test = JsonSerializer.Serialize<Character>(Terrarian);
+
+File.WriteAllText("char.json", test);
+
+Console.WriteLine(test);
+
+Console.ReadLine();
 
